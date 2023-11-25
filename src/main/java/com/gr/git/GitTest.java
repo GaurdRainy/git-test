578decb4fe452fb813373f96139cd8c73749c278
jiayi <343272606@qq.com>
2023-11-25 22:04:04 +0800

    hot fix commit 2

--- src/main/java/com/gr/git/GitTest.java
+++ src/main/java/com/gr/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello some change for checkout");
         System.out.println("hello some change for checkout2");
         System.out.println("change in hot-fix normal merge");
+        System.out.println("change in hot-fix conflict merge");
     }
 }
